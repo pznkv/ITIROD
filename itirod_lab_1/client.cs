7367de1dbcf825ae3703c4ce013b48114d05656b
pznkv <57153340+pznkv@users.noreply.github.com>
2022-03-01 12:37:40 +0300

    lab 1
    here only 2 files, server and client for your convenience. 4 var

---
+++ itirod_lab_1/client.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDP_CHAT_CLIENT
+{
+    class Program
+    {
+        static int remotePort; // Порт для отправки сообщений
+        static IPAddress ipAddress; // IP адрес сервера
+        static Socket listeningSocket; // Сокет
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("UDP CHAT CLIENT VERSION 3");
+            Console.Write("Введите ip адрес получателя: ");
+            ipAddress = IPAddress.Parse(Console.ReadLine());
+            Console.Write("Введите порт для отправки сообщений: ");
+            remotePort = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Для отправки сообщений введите сообщение и нажмите Enter");
+            Console.WriteLine();
+
+            try
+            {
+                listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp); // Создание сокета
+                Task listeningTask = new Task(Listen); // Создание потока
+                listeningTask.Start(); // Запуск потока
+
+                while (true) // Отправление сообщений серверу в бесконечном цикле
+                {
+                    string message = Console.ReadLine();
+
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    EndPoint remotePoint = new IPEndPoint(ipAddress, remotePort);
+                    listeningSocket.SendTo(data, remotePoint);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Close();
+            }
+        }
+
+        // Поток для приема подключений
+        private static void Listen()
+        {
+            try
+            {
+                IPEndPoint localIP = new IPEndPoint(IPAddress.Parse("0.0.0.0"), 0); // Прослушиваем по адресу
+                listeningSocket.Bind(localIP);
+
+                while (true)
+                {
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    byte[] data = new byte[256];
+
+                    EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0);
+
+                    do
+                    {
+                        bytes = listeningSocket.ReceiveFrom(data, ref remoteIp);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (listeningSocket.Available > 0);
+
+                    IPEndPoint remoteFullIp = remoteIp as IPEndPoint;
+
+                    Console.WriteLine("{0}:{1} - {2}", remoteFullIp.Address.ToString(), remoteFullIp.Port, builder.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Close();
+            }
+        }
+        // закрытие сокета
+        private static void Close()
+        {
+            if (listeningSocket != null)
+            {
+                listeningSocket.Shutdown(SocketShutdown.Both);
+                listeningSocket.Close();
+                listeningSocket = null;
+            }
+
+            Console.WriteLine("Сервер остановлен!");
+        }
+    }
+}
